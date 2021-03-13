4aa7acfc78a38dd31645093470623d0e05de76e7
Andrew Wong <andrew.j.wong@outlook.com>
2021-03-13 20:13:33 +1100

    Remove leading and trailing spaces

--- src/conversions/linesJSON.ts
+++ src/conversions/linesJSON.ts
@@ -3,7 +3,8 @@ import type { Conversion } from "../types/Conversion";
 const conversion: Conversion = {
   name: "Lines to JSON",
   aToB: function (input) {
-    return JSON.stringify(input.split("\n"), null, 4)
+    input = input.trim()
+    return input ? JSON.stringify(input.split("\n"), null, 4) : "[]"
   },
   bToA: function (input) {
     return JSON.parse(input).join("\n")
