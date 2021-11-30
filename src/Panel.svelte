c20e87eadf26cba7116d2d4f1b6ffd7947fb141c
Andrew Wong <andrew.j.wong@outlook.com>
2021-12-01 09:40:11 +1100

    Change border colour

--- src/Panel.svelte
+++ src/Panel.svelte
@@ -4,10 +4,16 @@
 
 <textarea bind:this={elem} on:keyup on:focus />
 
-<style>
+<style lang="scss">
   textarea {
     resize: none;
     width: 100%;
     height: 100%;
+
+    &:focus {
+      outline: none !important;
+      border: 1px solid var(--accent);
+      box-shadow: 0 0 5px var(--accent2);
+    }
   }
 </style>
