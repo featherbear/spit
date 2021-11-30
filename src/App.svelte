ad0140e5fbe6f24c444a429359661d2cef34243a
Andrew Wong <andrew.j.wong@outlook.com>
2021-12-01 09:34:48 +1100

    Housekeeping

--- src/App.svelte
+++ src/App.svelte
@@ -36,13 +36,16 @@
     }
   }
 
-  function handleFormat(field: HTMLInputElement, operation: ConversionFunction) {
+  function handleFormat(
+    field: HTMLInputElement,
+    operation: ConversionFunction
+  ) {
     try {
-        field.value = operation(field.value);
-      } catch (e) {
-        field.placeholder = e.message;
-        field.value = "";
-      }
+      field.value = operation(field.value);
+    } catch (e) {
+      field.placeholder = e.message;
+      field.value = "";
+    }
   }
 </script>
 
@@ -56,10 +59,15 @@
   </header>
   <main>
     <div class="panel">
-      <h6>{conversionOption && conversions[conversionOption].aName}
-      {#if conversions[conversionOption]?.aFormat}
-        <button on:click={() => handleFormat(inputA, conversions[conversionOption].aFormat)}>Format</button>
-      {/if}
+      <h6>
+        {conversionOption && conversions[conversionOption].aName}
+        {#if conversions[conversionOption]?.aFormat}
+          <button
+            on:click={() =>
+              handleFormat(inputA, conversions[conversionOption].aFormat)}
+            >Format</button
+          >
+        {/if}
       </h6>
       <Panel
         on:keyup={handleChange}
@@ -77,10 +85,15 @@
       </select>
     </div>
     <div class="panel">
-      <h6>{conversionOption && conversions[conversionOption].bName}
-      {#if conversions[conversionOption]?.bFormat}
-        <button on:click={() => handleFormat(inputB, conversions[conversionOption].bFormat)}>Format</button>
-      {/if}
+      <h6>
+        {conversionOption && conversions[conversionOption].bName}
+        {#if conversions[conversionOption]?.bFormat}
+          <button
+            on:click={() =>
+              handleFormat(inputB, conversions[conversionOption].bFormat)}
+            >Format</button
+          >
+        {/if}
       </h6>
       <Panel
         on:keyup={handleChange}
@@ -99,92 +112,90 @@
 
 <style lang="scss">
   div.container {
-
     --accent: #ff3e00;
     --accent2: #ff8a62;
     --accent3: #f7d5ca;
-    
+
     width: 100%;
     height: 100%;
     display: flex;
     flex-direction: column;
-  
 
-  header {
-    text-align: center;
-    padding: 1em;
-    // max-width: 240px;
-    margin: 0 auto;
+    header {
+      text-align: center;
+      padding: 1em;
+      // max-width: 240px;
+      margin: 0 auto;
 
-    h1 {
-      color: var(--accent);
-      text-transform: uppercase;
-      font-size: 3em;
-      font-weight: 100;
+      h1 {
+        color: var(--accent);
+        text-transform: uppercase;
+        font-size: 3em;
+        font-weight: 100;
+      }
+
+      p {
+        margin-top: 4px;
+        margin-bottom: 4px;
+      }
     }
 
-    p {
-      margin-top: 4px;
-      margin-bottom: 4px;
-    }
-  }
+    main {
+      flex: 1;
+      padding: 8px;
+      display: flex;
+      flex-direction: row;
 
-  main {
-    flex: 1;
-    padding: 8px;
-    display: flex;
-    flex-direction: row;
+      h6 {
+        font-size: 1em;
+        margin: 5px 0;
 
-    h6 {
-      font-size: 1em;
-      margin: 5px 0;
+        button {
+          margin: 0;
+          border: 2px solid var(--accent);
+          background-color: transparent;
+          cursor: pointer;
 
-      button {
-        margin: 0;
-        border: 2px solid var(--accent);
-        background-color: transparent;
-        cursor: pointer;
+          transition: background-color 0.1s;
 
-        transition: background-color 0.1s;
+          &:hover {
+            background-color: var(--accent3);
+          }
+        }
+      }
 
-        &:hover {
-          background-color: var(--accent3);
+      > :global(*) {
+        flex: 1;
+        text-align: center;
+        height: 100%;
+      }
+
+      > .panel {
+        display: flex;
+        flex-direction: column;
+      }
+
+      > .selector {
+        max-width: 20%;
+
+        > select {
+          text-align: center;
+          max-width: calc(100% - 20px);
         }
       }
     }
 
-    > :global(*) {
-      flex: 1;
+    footer {
+      background-color: var(--accent2);
+      padding: 4px;
+      color: white;
+      user-select: none;
       text-align: center;
-      height: 100%;
     }
-
-    > .panel {
-      display: flex;
-      flex-direction: column;
-    }
-
-    > .selector {
-      max-width: 20%;
-
-      > select {
-        text-align: center;
-        max-width: calc(100% - 20px);
-      }
-    }
-  }
-
-  footer {
-    background-color: var(--accent2);
-    padding: 4px;
-    color: white;
-    user-select: none;
-    text-align: center;
-  }
-  //   @media (min-width: 640px) {
-  //     main {
-  //       max-width: none;
-  //     }
-  //   }
+    //   @media (min-width: 640px) {
+    //     main {
+    //       max-width: none;
+    //     }
+    //   }
   }
 </style>
